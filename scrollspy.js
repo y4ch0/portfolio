53949aa128c3fa533c0469de69cd521efda3d445
y4ch0 <82388185+y4ch0@users.noreply.github.com>
2025-05-06 22:53:41 +0200

    Add files via upload

---
+++ scrollspy.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll(".links li a");
+    const sections = Array.from(links).map((link) => document.querySelector(link.getAttribute("href")));
+
+    const observerOptions = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.75, // minimum 60% widoczności sekcji
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const id = entry.target.getAttribute("id");
+
+                links.forEach((link) => {
+                    link.parentElement.classList.remove("current");
+                    if (link.getAttribute("href") === `#${id}`) {
+                        link.parentElement.classList.add("current");
+                    }
+                });
+            }
+        });
+    }, observerOptions);
+
+    sections.forEach((section) => {
+        if (section) observer.observe(section);
+    });
+});
